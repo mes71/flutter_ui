216b3f3786a16fe478fa1b67ecb54138f9d0b9ca
mr.mojtaba <mr.mojtaba47@gmail.com>
2024-02-09 23:01:18 +0330

    chore => color utils and some utils

---
+++ lib/ui/screen/screen.dart
@@ -0,0 +1,1 @@
+export 'nike_product_card/nike_product_card.dart';
---
+++ lib/ui/utils/color_convertor.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+MaterialColor hexToMaterialColor(String hexColor) {
+  // Remove the '#' character from the beginning of the hex color
+  hexColor = hexColor.replaceAll('#', '');
+
+  // Parse the hex color string to get the individual color values
+  int hexValue = int.parse(hexColor, radix: 16);
+
+  // Extract the individual color components
+  int alpha = (hexValue >> 24) & 0xFF;
+  int red = (hexValue >> 16) & 0xFF;
+  int green = (hexValue >> 8) & 0xFF;
+  int blue = hexValue & 0xFF;
+
+  // Create a MaterialColor object
+  return MaterialColor(
+    hexValue,
+    <int, Color>{
+      50: Color.fromRGBO(red, green, blue, 0.1),
+      100: Color.fromRGBO(red, green, blue, 0.2),
+      200: Color.fromRGBO(red, green, blue, 0.3),
+      300: Color.fromRGBO(red, green, blue, 0.4),
+      400: Color.fromRGBO(red, green, blue, 0.5),
+      500: Color.fromRGBO(red, green, blue, 0.6),
+      600: Color.fromRGBO(red, green, blue, 0.7),
+      700: Color.fromRGBO(red, green, blue, 0.8),
+      800: Color.fromRGBO(red, green, blue, 0.9),
+      900: Color.fromRGBO(red, green, blue, 1.0),
+    },
+  );
+}
