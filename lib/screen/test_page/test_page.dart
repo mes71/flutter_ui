3a5075ece529dd88ea324e51eeaa2c60a2f383e4
mr.mojtaba <mr.mojtaba47@gmail.com>
2024-07-16 18:49:53 +0330

    chore : update .gitignore

---
+++ lib/screen/test_page/test_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TestPage extends StatefulWidget {
+  const TestPage({super.key});
+
+  @override
+  State<TestPage> createState() => _TestPageState();
+}
+
+class _TestPageState extends State<TestPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
