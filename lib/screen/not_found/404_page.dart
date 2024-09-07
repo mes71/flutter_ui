e88251fe7cf0190311a5bbe15cdfecb93a7c0a59
mojtaba <mr.mojtaba47@gmail.com>
2024-09-07 18:26:54 +0330

    feat : init not found page

---
+++ lib/screen/not_found/404_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NotFoundPage extends StatefulWidget {
+  const NotFoundPage({super.key});
+
+  @override
+  State<NotFoundPage> createState() => _NotFoundPageState();
+}
+
+class _NotFoundPageState extends State<NotFoundPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
